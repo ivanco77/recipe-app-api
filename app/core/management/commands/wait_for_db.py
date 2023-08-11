c2735f5f5caad92b7ad6c4a6b2afb148b4455c96
Ivan Tymofiiev <ivanco77@gmail.com>
2023-08-11 22:44:17 +0300

    flake errorrs fixed

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,6 +10,7 @@ from django.db.utils import OperationalError
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """ Django command to wait for database."""
     def handle(self, *args, **options):
@@ -24,4 +25,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable. Waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-    
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Test custom Django management commands.
 """
 
@@ -10,11 +10,10 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check')
 
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """ Test Commands. """
-
     def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for database if database ready"""
         patched_check.return_value = True
@@ -24,7 +23,6 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for database when getting OperationalError. """
         patched_check.side_effect = [Psycopg2Error] * 2 + \
